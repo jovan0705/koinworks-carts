768f366f393aa9273e6b9ce9123d9be1ab922be7
jovan0705 <jovan0705@gmail.com>
2022-02-05 18:50:12 +0700

    add, remove product to cart and updating item counts done

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,28 @@
+const errorHandler = (err, req, res, next) => {
+    switch (err.name) {
+        case "PRODUCT_NOT_FOUND":
+            res.status(404).json({message: 'Product not Found'})
+            break;
+        
+        case "NO_STOCK": 
+            res.status(400).json({message: 'This item is currently not available'})
+            break;
+
+        case "ITEM_COUNT_MIN_1":
+            res.status(400).json({message: 'Minimum count 1 item'})
+            break;
+            
+        case "PRODUCT_ALREADY_ADDED":
+            res.status(400).json({message: 'Product already added'})
+            break;
+
+        case "ITEM_NOT_IN_CART": 
+            res.status(400).json({message: 'Item not found in Cart'})
+            break;
+        default:
+            res.status(500).json(err)
+            break;
+    }
+}
+
+module.exports = errorHandler
--- router/cartRouter.js
+++ router/cartRouter.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
+const cartController = require('../controller/cartController')
 
-
+router.post('/create', cartController.createCart)
+router.post('/product/:productId', cartController.addItem)
+router.delete('/product/:productId', cartController.removeItem)
+router.patch('/product/:productId/increase', cartController.increaseItem)
+router.patch('/product/:productId/decrease', cartController.decreaseItem)
 
 module.exports = router
